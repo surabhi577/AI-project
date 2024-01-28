e2cefb37b0c79d893e93d536a159b12f3de8addf
surabhi577 <95013898+surabhi577@users.noreply.github.com>
2024-01-29 00:44:41 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,20 +1,20 @@
 import streamlit as st
 from dotenv import load_dotenv
-from PyPDF2 import PdfFileReader  
+from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template  # Corrected import
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
-        pdf_reader = PdfFileReader(pdf) 
-        for page in range(pdf_reader.numPages):  
-            text += pdf_reader.getPage(page).extractText()  
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
     return text
 
 def get_text_chunks(text):
