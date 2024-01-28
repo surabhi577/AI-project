f4ba94f3b2df3b29e3af3edcb3442a78b06afbac
surabhi577 <95013898+surabhi577@users.noreply.github.com>
2024-01-29 00:21:52 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
+from PyPDF2 import pdfFileReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
